1f4a108eed5efce69415954564ba8083a6920c0b
p1nero <1070402601@qq.com>
2025-12-03 21:19:15 +0800

    升3.2.2

---
+++ src/main/java/com/p1nero/tcrmodfilter/service/TCRPreloadingEntrypoint.java
@@ -0,0 +1,35 @@
+package com.p1nero.tcrmodfilter.service;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.loading.ImmediateWindowHandler;
+import org.slf4j.Logger;
+import settingdust.preloading_tricks.api.PreloadingEntrypoint;
+import settingdust.preloading_tricks.api.PreloadingTricksCallbacks;
+
+import java.util.Set;
+
+public class TCRPreloadingEntrypoint implements PreloadingEntrypoint {
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TCRPreloadingEntrypoint() {
+        PreloadingTricksCallbacks.SETUP_MODS.register(modManager -> {
+            String glVersion = ImmediateWindowHandler.getGLVersion();
+            String[] parts = glVersion.split("\\.");
+            int major;
+            int minor;
+            major = Integer.parseInt(parts[0]);
+            minor = Integer.parseInt(parts[1]);
+            if(major < 4 || (major == 4 && minor < 6)) {
+                if(modManager.removeById("acceleratedrendering")){
+                    LOGGER.warn("GLVersion < 4.6, acceleratedrendering mod has been removed.");
+                }
+            }
+            if(modManager.removeById("yes_steve_model")){
+                LOGGER.warn("Incompatible with epic fight, yes_steve_model mod has been removed.");
+            }
+            modManager.removeByIds(Set.of("projecte"));
+        });
+    }
+
+}
